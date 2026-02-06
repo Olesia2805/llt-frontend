3a213eb8493c1b2fdb53a9ba3fa4fa760800efd5
Olesia Vysotina <vysotina2002@gmail.com>
2026-02-06 22:22:45 +0100

    remove unnecessary overlay div from Loader component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,7 @@
 import { ClimbingBoxLoader } from "react-spinners";
-import styles from "./Loader.module.css";
 
 const Loader = () => {
-  return (
-    <div className={styles.overlay}>
-      <ClimbingBoxLoader color="var(--primary)" />
-    </div>
-  );
+  return <ClimbingBoxLoader color="var(--primary)" />;
 };
 
 export default Loader;
